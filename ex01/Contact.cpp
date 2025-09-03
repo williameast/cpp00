def1e7788cfa556540f08275f8dab2d835b1787a
William East <weast@c3a3c4.42berlin.de>
2025-09-03 12:37:02 +0200

    added number check

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,7 @@
 //   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/08/14 12:39:46 by weast             #+#    #+#             //
-//   Updated: 2025/08/14 17:07:58 by weast            ###   ########.fr       //
+//   Updated: 2025/08/20 15:28:01 by weast            ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -52,7 +52,6 @@ std::string Contact::get_secret(void) const
 	return(this->_secret);
 }
 
-
 // setters
 void	Contact::set_name(std::string str)
 {
@@ -95,12 +94,45 @@ static std::string get_input(const std::string &message)
     }
 }
 
+static bool is_valid(const std::string& s) {
+    for (size_t i = 0; i < s.length(); ++i) {
+        char c = s[i];
+        if (!((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '(' || c == ')')) {
+            return false;
+        }
+    }
+	return true;
+}
+
+static std::string get_numeric_input(const std::string &message)
+{
+    std::string input;
+
+    while (true)
+    {
+        std::cout << message;
+		if (!std::getline(std::cin, input))
+			{
+				std::cout << "\nInput cancelled (EOF).\n";
+				return "";
+			}
+        if (!input.empty())
+		{
+			if (is_valid(input))
+				return input;
+			std::cout << "Phone number must consist of 0-9, + - (  ). Try again.\n";
+		}
+		if (input.empty())
+			std::cout << "Field cannot be empty. Try again.\n";
+    }
+}
+
 void Contact::capture_contact()
 {
     set_name(get_input("First name: "));
     set_surname(get_input("Last name: "));
     set_nick(get_input("Nickname: "));
-    set_number(get_input("Phone Number: "));
+    set_number(get_numeric_input("Phone Number: "));
     set_secret(get_input("Secret: "));
 
     std::cout << get_full_name() + " added successfully.\n";
